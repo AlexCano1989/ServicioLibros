f21b94170fa255ba1d8e9aafc8c78a782f0987f7
AlexCano1989 <148381903+AlexCano1989@users.noreply.github.com>
2023-12-20 15:03:20 -0500

    Flux commit

--- src/main/java/com/libros/libros/repositories/LibroRepository.java
+++ src/main/java/com/libros/libros/repositories/LibroRepository.java
@@ -3,8 +3,19 @@ package com.libros.libros.repositories;
 import com.libros.libros.models.Libro;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
-public interface LibroRepository extends ReactiveCrudRepository<Libro, Long> {
+
+public interface LibroRepository extends R2dbcRepository<Libro, Long> {
 
     Flux<Libro> findByAutor(String autor);
+
+    Flux<Libro> findAll();
+
+    Mono<Libro> findById(Long id);
+
+    Mono<Libro> save(Libro libro);
+
+    Mono<Void> deleteById(Long id);
 }
